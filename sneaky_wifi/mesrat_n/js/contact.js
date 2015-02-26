1c95523706952c77400526ecb02006e5f0e06272
mesratn <mesrat_n@etna-alternance.net>
2015-02-26 16:13:52 +0100

    Fichiers non suivis: 	battel_for_midgard/ 	ecommerce/ 	micro_shell/ 	morpion/ 	sneaky_wifi/ 	tivine/ 	tivine_twitter/

--- sneaky_wifi/mesrat_n/js/contact.js
+++
@@ -1,9 +0,0 @@
-setTimeout(function(){var parisLatlng = new google.maps.LatLng(48.813244, 2.393041);
-        var mapOptions = {
-                center: parisLatlng,
-                zoom: 16
-        };
-    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-    $('#map').show();}, 300);
-
-var button = document.getElementByClass('btn btn-action');
